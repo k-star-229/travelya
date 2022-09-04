eb1d28e7e0e5ce21cd2275e7e6551644c4270ac3
ps-lightning <ps.lightning.workplace@gmail.com>
2022-09-04 08:18:32 -0400

    logo added

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+import tw from "twin.macro";
+
+const LogoContainer = styled.div`
+  ${tw`
+    font-family["ZCOOL KuaiLe"]
+    font-black
+    text-2xl
+    text-white
+  `};
+`;
+
+export function Logo() {
+  return <LogoContainer></LogoContainer>
+}
